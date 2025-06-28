d23faca2384042c9649f728bb3509251752d9db5
pika2105chu <dguruharipriya@gmail.com>
2025-06-28 21:45:04 +0530

    added a new line to learn pushing

--- hello.py
+++ hello.py
@@ -2,3 +2,6 @@ print("hello rough work")
 print("okay so the time and date is \n 3:13pm \n 28-06-2025")
 print("hmph, lets see now and make other projects")
 print("but now its 9:24pm, 28/06/2025")
+
+# huff
+print("yeah I guess the pushing the code and moving the folder to another is done, lets try again")
