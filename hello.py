b185987e966185d14e16d8c0f6371fd523026386
pika2105chu <dguruharipriya@gmail.com>
2025-06-28 21:32:33 +0530

    Move the hello.py into the folder pyhton(rough) which is in now rough_projects

--- hello.py
+++ hello.py
@@ -1,1 +1,4 @@
 print("hello rough work") 
+print("okay so the time and date is \n 3:13pm \n 28-06-2025")
+print("hmph, lets see now and make other projects")
+print("but now its 9:24pm, 28/06/2025")
